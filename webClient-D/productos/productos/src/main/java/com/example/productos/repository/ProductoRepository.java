55b6b7ee2c3ecc84de0d69bcffa89ee47f6df726
mardram <vi.krausse@duocuc.cl>
2026-05-05 08:42:47 -0400

    Delete webClient-D directory

--- webClient-D/productos/productos/src/main/java/com/example/productos/repository/ProductoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.productos.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-
-}
