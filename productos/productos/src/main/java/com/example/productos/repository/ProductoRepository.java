f8bd394333ef7f63951fa0c695287ad83340806d
mardram <vi.krausse@duocuc.cl>
2026-05-05 09:01:47 -0400

    Delete productos directory

--- productos/productos/src/main/java/com/example/productos/repository/ProductoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.productos.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.productos.model.Producto; // <-- ESTA ES LA LÍNEA QUE FALTA
-
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-}
